e46a122b3f11476d4122be87db1e0014a3cff49f
n42353fp <n42353fp@mac.home.arpa>
2026-07-24 13:53:34 +0100

    Add F<->E interconversion in ISF model

--- methods/models.py
+++ methods/models.py
@@ -391,6 +391,138 @@ class HPAModelFEInterCBGAlbBloodISF(pints.ForwardModel):
         k_6 = self.parameters['k_6']
         k_7 = self.parameters['k_7']
         k_8 = self.parameters['k_8']
+        k_BI = self.parameters['k_BI'] # new param
+        V_B = self.parameters['V_B'] # new param
+        V_I = self.parameters['V_I'] # new param
+        alpha = self.parameters['alpha']
+        delay = self.parameters['delay']
+        lambda_s = self.parameters['lambda_s']
+        lambda_a = self.parameters['lambda_a']
+        t_s = self.parameters['t_s']
+        m_a = self.parameters['m_a']
+        m_f = self.parameters['m_f']
+        sigma = self.parameters['sigma']
+        gamma_a = self.parameters['gamma_a']
+        gamma_f_b = self.parameters['gamma_f_b']
+        gamma_e_b = self.parameters['gamma_e_b']
+        gamma_f_i = self.parameters['gamma_f_i'] # new param
+        gamma_e_i = self.parameters['gamma_e_i'] # new param
+
+        # Define the DDE model
+        def model(Y, t):
+            A, F_B, E_B, F_CBG, F_Alb, E_CBG, E_Alb, CBG, Alb, F_I, E_I = Y(t)
+            F_delay = Y(t - delay)[1]
+
+            dAdt = -gamma_a*A + ((K_f**m_a)*self.crh(t, t_s, lambda_a, lambda_s, sigma))/(K_f**m_a+F_delay**m_a)
+            dF_Bdt = -(gamma_f_b+k_1*CBG+k_3*Alb)*F_B + alpha*((A**m_f)/(K_a**m_f + A**m_f)) + k_2*F_CBG + k_4*F_Alb + (V_e*E_B)/(k_me+E_B) - (V_f+F_B)/(k_mf+F_B) - (k_BI/V_B)*(F_B-F_I)
+            dE_Bdt = -(gamma_e_b+k_5*CBG+k_7*Alb)*E_B + k_6*E_CBG + k_8*E_Alb - (V_e*E_B)/(k_me+E_B) + (V_f+F_B)/(k_mf+F_B) - (k_BI/V_B)*(E_B-E_I)
+            dF_CBGdt = k_1*F_B*CBG - k_2*F_CBG
+            dF_Albdt = k_3*F_B*Alb - k_4*F_Alb
+            dE_CBGdt = k_5*E_B*CBG - k_6*E_CBG
+            dE_Albdt = k_7*E_B*Alb - k_8*E_Alb
+            dCBGdt = k_2*F_CBG - k_1*F_B*CBG + k_6*E_CBG - k_5*E_B*CBG
+            dAlbdt = k_4*F_Alb - k_3*F_B*Alb + k_8*E_Alb - k_7*E_B*Alb
+            dF_Idt = (k_BI/V_I)*(F_B-F_I) - gamma_f_i*F_I
+            dE_Idt = (k_BI/V_I)*(E_B-E_I) - gamma_e_i*E_I
+
+            return [dAdt, dF_Bdt, dE_Bdt, dF_CBGdt, dF_Albdt, dE_CBGdt, dE_Albdt, dCBGdt, dAlbdt, dF_Idt, dE_Idt]
+
+        # Define initial conditions
+        def initial_conditions(t):
+            return [5, 10, 1, 0, 0, 0, 0, 20, 40000, 0, 0]
+        
+        # Run the simulation     
+        result = ddeint(model, initial_conditions, self.times)
+
+        # Truncate to specified range
+        result = result[int((self.length_model/self.step)*(self.num_days-self.days_to_keep)):]
+
+        # Find nearest indices
+        indices = np.searchsorted(self.times, times)
+
+        # Pull the filtered values
+        filtered_output = result[indices]
+
+        return filtered_output
+
+    def n_outputs(self):
+        return 11
+
+    def n_times(self):
+        return self.length_model*self.num_days
+
+    def n_parameters(self):
+        return self.n_parameters_value
+
+    def get_and_create_boundaries(self):
+        lowerbounds = []
+        upperbounds = []
+
+        for item in self.parameters.keys():
+            lowerbound, upperbound = self.parameter_boundaries.get(item, (None, None))
+            if lowerbound is None or upperbound is None: 
+                print(f'{item} has no defined bounds. Setting to default (0, 1000)')
+                lowerbound, upperbound = (0, 1000)
+            
+            lowerbounds.append(lowerbound)
+            upperbounds.append(upperbound)
+        
+        return pints.RectangularBoundaries(lowerbounds, upperbounds)
+
+class HPAModelFEInterBothCBGAlbBloodISF(pints.ForwardModel):
+    def __init__(self,
+                 parameters,
+                 times,
+                 num_days=6,
+                 days_to_keep=1,
+                 step=0.1):
+        self.num_days = num_days
+        self.days_to_keep = days_to_keep
+        self.step = step
+        self.n_parameters_value = len(parameters)
+        self.parameters = parameters
+        self.times = times
+        self.length_model = day_len
+        self.parameter_boundaries = PARAMETER_BOUNDARIES.copy()
+
+    def crh(self, t, t_s=None, lambda_a=None, lambda_s=None, sigma=None, T_c=day_len, symmetric=False):
+        if t_s is None: 
+            t_s = self.parameters['t_s']
+        if lambda_a is None: 
+            lambda_a = self.parameters['lambda_a']
+        if lambda_s is None: 
+            lambda_s = self.parameters['lambda_s']
+        if sigma is None: 
+            sigma = self.parameters['sigma']
+        crh = lambda_a * math.e**(lambda_s*math.cos(2*math.pi*((t-t_s)/T_c)+sigma*math.cos(2*math.pi*((t-t_s)/T_c))))
+        if symmetric:
+            # Arbitrary single cosine csh drive (for testing)
+            crh = 70*math.cos(2*math.pi*(t/T_c))+75
+        return crh
+
+    def simulate(self, parameters, times):
+        
+        # Assign parameters
+        param_keys = list(self.parameters.keys())
+        for i, key in enumerate(param_keys):
+            self.parameters[key] = parameters[i]
+
+        K_a = self.parameters['K_a']
+        K_f = self.parameters['K_f']
+        k_mf = self.parameters['k_mf']
+        k_me = self.parameters['k_me']
+        V_f_b = self.parameters['V_f_b']
+        V_e_b = self.parameters['V_e_b']
+        V_f_i = self.parameters['V_f_i'] # new param
+        V_e_i = self.parameters['V_e_i'] # new param
+        k_1 = self.parameters['k_1']
+        k_2 = self.parameters['k_2']
+        k_3 = self.parameters['k_3']
+        k_4 = self.parameters['k_4']
+        k_5 = self.parameters['k_5']
+        k_6 = self.parameters['k_6']
+        k_7 = self.parameters['k_7']
+        k_8 = self.parameters['k_8']
         k_BI = self.parameters['k_BI']
         V_B = self.parameters['V_B']
         V_I = self.parameters['V_I']
@@ -414,16 +546,16 @@ class HPAModelFEInterCBGAlbBloodISF(pints.ForwardModel):
             F_delay = Y(t - delay)[1]
 
             dAdt = -gamma_a*A + ((K_f**m_a)*self.crh(t, t_s, lambda_a, lambda_s, sigma))/(K_f**m_a+F_delay**m_a)
-            dF_Bdt = -(gamma_f_b+k_1*CBG+k_3*Alb)*F_B + alpha*((A**m_f)/(K_a**m_f + A**m_f)) + k_2*F_CBG + k_4*F_Alb + (V_e*E_B)/(k_me+E_B) - (V_f+F_B)/(k_mf+F_B) - (k_BI/V_B)*(F_B-F_I)
-            dE_Bdt = -(gamma_e_b+k_5*CBG+k_7*Alb)*E_B + k_6*E_CBG + k_8*E_Alb - (V_e*E_B)/(k_me+E_B) + (V_f+F_B)/(k_mf+F_B) - (k_BI/V_B)*(E_B-E_I)
+            dF_Bdt = -(gamma_f_b+k_1*CBG+k_3*Alb)*F_B + alpha*((A**m_f)/(K_a**m_f + A**m_f)) + k_2*F_CBG + k_4*F_Alb + (V_e_b*E_B)/(k_me+E_B) - (V_f_b+F_B)/(k_mf+F_B) - (k_BI/V_B)*(F_B-F_I)
+            dE_Bdt = -(gamma_e_b+k_5*CBG+k_7*Alb)*E_B + k_6*E_CBG + k_8*E_Alb - (V_e_b*E_B)/(k_me+E_B) + (V_f_b+F_B)/(k_mf+F_B) - (k_BI/V_B)*(E_B-E_I)
             dF_CBGdt = k_1*F_B*CBG - k_2*F_CBG
             dF_Albdt = k_3*F_B*Alb - k_4*F_Alb
             dE_CBGdt = k_5*E_B*CBG - k_6*E_CBG
             dE_Albdt = k_7*E_B*Alb - k_8*E_Alb
             dCBGdt = k_2*F_CBG - k_1*F_B*CBG + k_6*E_CBG - k_5*E_B*CBG
             dAlbdt = k_4*F_Alb - k_3*F_B*Alb + k_8*E_Alb - k_7*E_B*Alb
-            dF_Idt = (k_BI/V_I)*(F_B-F_I) - gamma_f_i*F_I
-            dE_Idt = (k_BI/V_I)*(E_B-E_I) - gamma_e_i*E_I
+            dF_Idt = (k_BI/V_I)*(F_B-F_I) - gamma_f_i*F_I + (V_e_i*E_I)/(k_me+E_I) - (V_f_i+F_I)/(k_mf+F_I)
+            dE_Idt = (k_BI/V_I)*(E_B-E_I) - gamma_e_i*E_I - (V_e_i*E_I)/(k_me+E_I) + (V_f_i+F_I)/(k_mf+F_I)
 
             return [dAdt, dF_Bdt, dE_Bdt, dF_CBGdt, dF_Albdt, dE_CBGdt, dE_Albdt, dCBGdt, dAlbdt, dF_Idt, dE_Idt]
 
---
+++ src/run_HPAModelFEInterBothCBGAlbBloodISF.py
@@ -0,0 +1,87 @@
+import os
+import sys
+top_level_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, top_level_dir)
+import numpy as np
+from matplotlib import pyplot as plt
+import json
+from methods.models import HPAModelFEInterBothCBGAlbBloodISF
+from methods import day_len
+
+# Define parameter config file path
+config_file = 'configs/HPAModelFEInterBothCBGAlbBloodISF/test_parameters.json'
+
+# Load config
+with open(config_file, 'r') as f:
+    config = json.load(f)
+
+# Get parameters from config
+parameters = config.get('parameters', {})
+num_days = 6
+days_to_keep = 1
+
+# Create time array
+timesteps = day_len * num_days
+step = 0.1 # stepsize must be sufficiently small for convergence of dde solver
+if day_len/step != int(day_len/step):
+    print(f"Warning: day_len ({day_len}) is not divisible by step ({step}). This may cause issues when plotting.")
+times = np.arange(0, timesteps, step)
+
+# Initialise model
+dde_model = HPAModelFEInterBothCBGAlbBloodISF(parameters=parameters, times=times, num_days=num_days, days_to_keep=days_to_keep, step=step)
+
+# Get plotting times (some cropping may occur if day_len/step != whole number)
+plot_times = times[int((day_len/step)*(num_days-days_to_keep)):] - (day_len)*(num_days-days_to_keep)
+
+# Run the simulation
+print("Running simulation...")       
+result = dde_model.simulate(list(parameters.values()), plot_times)
+print("Simulation complete.")
+
+# Plot output
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 12))
+
+F_tot = result.T[1]+result.T[3]+result.T[4]
+E_tot = result.T[2]+result.T[5]+result.T[6]
+
+ax1.plot(plot_times, F_tot, label='Total Cortisol', color='blue')
+ax1.plot(plot_times, result.T[1], label='Free Cortisol', color='green')
+ax1.plot(plot_times, E_tot, label='Total Cortisone', color='red')
+ax1.plot(plot_times, result.T[2], label='Free Cortisone', color='yellow')
+ax2.plot(plot_times, result.T[9], label='Free Cortisol', color='blue', alpha=0.5)
+ax2.plot(plot_times, result.T[10], label='Free Cortisone', color='red', alpha=0.5)
+ax3.plot(plot_times, result.T[0], label='ACTH', color='orange')
+ax1.set_ylabel('nmol/L')
+ax2.set_ylabel('nmol/L')
+ax3.set_ylabel('pmol/L')
+ax3.set_xlabel('Time (minutes)')
+
+for ax in [ax1, ax2, ax3]:
+    ax.set_xlim(plot_times[0], plot_times[-1])
+    for i in range(days_to_keep):
+        ax.axvline(x=day_len*(i+num_days-days_to_keep), color='gray', linestyle='--') 
+
+crh_drive = [dde_model.crh(t) for t in plot_times]
+
+ax4 = ax1.twinx()
+ax4.plot(plot_times, crh_drive, color = 'grey', alpha = 0.4)
+ax4.set_ylabel('CRH drive', color = 'grey')
+
+ax5 = ax2.twinx()
+ax5.plot(plot_times, crh_drive, color = 'grey', alpha = 0.4)
+ax5.set_ylabel('CRH drive', color = 'grey')
+
+ax6 = ax3.twinx()
+ax6.plot(plot_times, crh_drive, color = 'grey', alpha = 0.4)
+ax6.set_ylabel('CRH drive', color = 'grey')
+
+ax1.set_title('Cortisol and Cortisone in Blood Plasma')
+ax2.set_title('Cortisol and Cortisone in ISF')
+ax3.set_title('ACTH in Blood Plasma')
+
+ax1.legend()
+ax2.legend()
+ax3.legend()
+
+plt.suptitle('Cortisol, Cortisone and ACTH Levels Over Time')
+plt.savefig(f'figures/model_output/HPAModelFEInterBothCBGAlbBloodISF/test_sim.png')
