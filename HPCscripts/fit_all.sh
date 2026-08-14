ce62b623a418801bde8d8deb90424d22bf7b3058
n42353fp <n42353fp@B-LOSXYKX3M.local>
2026-08-14 15:55:01 +0100

    First HPC fitting bash scripts

--- HPCscripts/fit_all.sh
+++ HPCscripts/fit_all.sh
@@ -1,1 +1,12 @@
-a
+#!/bin/bash --login
+
+#SBATCH -p serial
+#SBATCH -t 20 
+#SBATCH -a 1-2
+
+module purge
+module load apps/binapps/conda/miniforge3/26.3.2-3
+
+conda activate myenv
+
+python3 src/fit_model.py --model 1 --ind ${SLURM_ARRAY_TASK_ID} --outdir 'test_fit_model'
